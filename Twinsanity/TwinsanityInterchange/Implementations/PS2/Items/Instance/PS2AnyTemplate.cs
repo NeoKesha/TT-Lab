2d34632a4257cb585de26799a7f6b0b6123150ad
Neo_Kesha <neokesha@yandex.ru>
2020-09-23 16:48:27 +0300

    Templates

--- Twinsanity/TwinsanityInterchange/Implementations/PS2/Items/Instance/PS2AnyTemplate.cs
+++ Twinsanity/TwinsanityInterchange/Implementations/PS2/Items/Instance/PS2AnyTemplate.cs
@@ -41,12 +41,46 @@ namespace Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Instance
 
         public int GetLength()
         {
-            return Constants.SIZE_VECTOR4;
+            return 4 + Name.Length + 16 + ((Header1 == 1)?2:0) + UnkFlags.Length + 4 + 12 + Flags.Count*4 + Floats.Count*4 + Ints.Count*4;
         }
 
         public void Read(BinaryReader reader, int length)
         {
-            
+            Int32 nameLen = reader.ReadInt32();
+            Name = new string(reader.ReadChars(nameLen));
+            ObjectId = reader.ReadUInt16();
+            Bitfield = reader.ReadUInt16();
+            Header1 = reader.ReadUInt32();
+            Header2 = reader.ReadUInt32();
+            Header3 = reader.ReadUInt32();
+            if (Header1 == 1)
+            {
+                UnkShort = reader.ReadUInt16();
+            }
+            for (int i = 0; i < 6; ++i)
+            {
+                UnkFlags[i] = reader.ReadByte();
+            }
+            Properties = reader.ReadUInt32();
+            Int32 flags = reader.ReadInt32();
+            Flags.Clear();
+            for (int i = 0; i < flags; ++i)
+            {
+                Flags.Add(reader.ReadUInt32());
+            }
+            Int32 floats = reader.ReadInt32();
+            Floats.Clear();
+            for (int i = 0; i < floats; ++i)
+            {
+                Floats.Add(reader.ReadSingle());
+            }
+            Int32 ints = reader.ReadInt32();
+            Ints.Clear();
+            for (int i = 0; i < ints; ++i)
+            {
+                Ints.Add(reader.ReadUInt32());
+            }
+            int len = GetLength();
         }
 
         public void SetID(uint id)
@@ -56,7 +90,37 @@ namespace Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Instance
 
         public void Write(BinaryWriter writer)
         {
-            
+            writer.Write(Name.Length);
+            writer.Write(Name.ToCharArray());
+            writer.Write(ObjectId);
+            writer.Write(Bitfield);
+            writer.Write(Header1);
+            writer.Write(Header2);
+            writer.Write(Header3);
+            if (Header1 == 1)
+            {
+                writer.Write(UnkShort);
+            }
+            for (int i = 0; i < 6; ++i)
+            {
+                writer.Write(UnkFlags[i]);
+            }
+            writer.Write(Properties);
+            writer.Write(Flags.Count);
+            foreach (UInt32 e in Flags)
+            {
+                writer.Write(e);
+            }
+            writer.Write(Floats.Count);
+            foreach (Single e in Floats)
+            {
+                writer.Write(e);
+            }
+            writer.Write(Ints.Count);
+            foreach (UInt32 e in Ints)
+            {
+                writer.Write(e);
+            }
         }
     }
 }
