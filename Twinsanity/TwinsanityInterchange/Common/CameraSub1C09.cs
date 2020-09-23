3dbb4d0e99de2851d8a55bf3101395edc2729d4c
Smartkin <vladislav5019@gmail.com>
2020-09-23 07:23:09 +0300

    Add Camera subtype 0x1C09

--- Twinsanity/TwinsanityInterchange/Common/CameraSub1C09.cs
+++ Twinsanity/TwinsanityInterchange/Common/CameraSub1C09.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,29 @@ namespace Twinsanity.TwinsanityInterchange.Common
 {
     class CameraSub1C09 : ITwinSerializable
     {
+        public UInt32 UnkInt { get; set; }
+        public Single UnkFloat1 { get; set; }
+        public Single UnkFloat2 { get; set; }
+        public CameraSub1C09()
+        {
+        }
+        public int GetLength()
+        {
+            return 12 + Constants.SIZE_VECTOR4;
+        }
+
+        public void Read(BinaryReader reader, int length)
+        {
+            UnkInt = reader.ReadUInt32();
+            UnkFloat1 = reader.ReadSingle();
+            UnkFloat2 = reader.ReadSingle();
+        }
+
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(UnkInt);
+            writer.Write(UnkFloat1);
+            writer.Write(UnkFloat2);
+        }
     }
 }
