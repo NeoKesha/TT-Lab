bfc2fd42e83c6d41d629d625a979e9ac4d566f6d
Smartkin <vladislav5019@gmail.com>
2020-09-24 12:48:22 +0300

    Add Animation section reading

--- Twinsanity/TwinsanityInterchange/Common/TwinBlob.cs
+++ Twinsanity/TwinsanityInterchange/Common/TwinBlob.cs
@@ -11,28 +11,32 @@ namespace Twinsanity.TwinsanityInterchange.Common
 {
     public class TwinBlob : ITwinSerializable
     {
-        UInt32 UnkBlobInt;
         UInt32 BlockSizePacked;
         UInt16 BlockSizeHelper;
         public Byte[] Blob { get; private set; }
         public int GetLength()
         {
-            return 10 + Blob.Length;
+            return 6 + (Blob != null ? Blob.Length : 0);
         }
 
         public void Read(BinaryReader reader, int length)
         {
-            UnkBlobInt = reader.ReadUInt32();
             BlockSizePacked = reader.ReadUInt32();
             BlockSizeHelper = reader.ReadUInt16();
-            Blob = reader.ReadBytes(GetBlockLength());
+            if (GetBlockLength() > 0)
+            {
+                Blob = reader.ReadBytes(GetBlockLength());
+            }
         }
 
         public void Write(BinaryWriter writer)
         {
             writer.Write(BlockSizePacked);
             writer.Write(BlockSizeHelper);
-            writer.Write(Blob);
+            if (GetBlockLength() > 0)
+            {
+                writer.Write(Blob);
+            }
         }
 
         Int32 GetBlockLength()
