73c581c1fb9a6cb955dfa5f5f6abae7f670bb9ae
Smartkin <vladislav5019@gmail.com>
2020-10-06 15:27:44 +0300

    Add Scenery reading

---
+++ Twinsanity/TwinsanityInterchange/Common/Lights/DirectionalLight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twinsanity.TwinsanityInterchange.Enumerations;
+
+namespace Twinsanity.TwinsanityInterchange.Common.Lights
+{
+    public class DirectionalLight : Light
+    {
+        public Vector4 UnkVec3;
+        public Int16 UnkShort;
+
+        public DirectionalLight() : base()
+        {
+            UnkVec3 = new Vector4();
+        }
+
+        public override Int32 GetLength()
+        {
+            return base.GetLength() + 2 + Constants.SIZE_VECTOR4;
+        }
+
+        public override void Read(BinaryReader reader, Int32 length)
+        {
+            base.Read(reader, length);
+            UnkVec3.Read(reader, Constants.SIZE_VECTOR4);
+            UnkShort = reader.ReadInt16();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            base.Write(writer);
+            UnkVec3.Write(writer);
+            writer.Write(UnkShort);
+        }
+    }
+}
---
+++ Twinsanity/TwinsanityInterchange/Common/ScenerySubtypes/SceneryRoot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twinsanity.TwinsanityInterchange.Common.ScenerySubtypes
+{
+    public class SceneryRoot : SceneryType_0x1600
+    {
+        public UInt32 UnkUInt;
+
+        public override Int32 GetLength()
+        {
+            return base.GetLength() + 4;
+        }
+
+        public override void Read(BinaryReader reader, Int32 length)
+        {
+            UnkUInt = reader.ReadUInt32();
+        }
+
+        public override void Read(BinaryReader reader, Int32 length, IList<SceneryBaseType> sceneries)
+        {
+            Read(reader, length);
+            base.Read(reader, length, sceneries);
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            writer.Write(UnkUInt);
+            base.Write(writer);
+        }
+    }
+}
