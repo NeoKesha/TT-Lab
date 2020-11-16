62b0ba40081f7c5a6114f72cb1bb7a39603a48a6
Smartkin <vladislav5019@gmail.com>
2020-11-16 14:45:29 +0300

    Set proper parent for PS2Default

--- Twinsanity/TwinsanityInterchange/Implementations/PS2/PS2Default.cs
+++ Twinsanity/TwinsanityInterchange/Implementations/PS2/PS2Default.cs
@@ -11,22 +11,11 @@ using Twinsanity.TwinsanityInterchange.Implementations.PS2.Sections.RM2;
 
 namespace Twinsanity.TwinsanityInterchange.Implementations.PS2
 {
-    public class PS2Default : BaseTwinSection
+    public class PS2Default : PS2AnyTwinsanityRM2
     {
         public PS2Default() : base()
         {
-            idToClassDictionary.Add(Constants.LEVEL_GRAPHICS_SECTION, typeof(PS2AnyGraphicsSection));
-            idToClassDictionary.Add(Constants.LEVEL_CODE_SECTION, typeof(PS2AnyCodeSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_1_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_2_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_3_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_4_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_5_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_6_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_7_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_LAYOUT_8_SECTION, typeof(PS2AnyLayoutSection));
-            idToClassDictionary.Add(Constants.LEVEL_PARTICLES_ITEM, typeof(PS2DefaultParticleData));
-            idToClassDictionary.Add(Constants.LEVEL_COLLISION_ITEM, typeof(BaseTwinItem));
+            idToClassDictionary[Constants.LEVEL_PARTICLES_ITEM] = typeof(PS2DefaultParticleData);
         }
     }
 }
