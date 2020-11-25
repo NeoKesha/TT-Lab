94958c2d7cb56573c84beec08d0ef4c657c0b7d2
Smartkin <vladislav5019@gmail.com>
2020-11-25 13:42:33 +0300

    Update texture reading

--- Twinsanity/TwinsanityInterchange/Implementations/PS2/Items/Graphics/PS2AnyTexture.cs
+++ Twinsanity/TwinsanityInterchange/Implementations/PS2/Items/Graphics/PS2AnyTexture.cs
@@ -16,14 +16,25 @@ namespace Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Graphics
         public UInt16 ImageWidthPower { get; set; }
         public UInt16 ImageHeightPower { get; set; }
         public Byte MipLevels { get; set; }
-        public Byte[] Header { get; private set; }
-        public Byte[] Metadata {get; private set; }
+        public TexturePixelFormat TextureFormat { get; private set; }
+        public TexturePixelFormat DestinationTextureFormat { get; private set; }
+        public TextureColorComponent ColorComponent;
+        public Byte UnkByte;
+        public TextureFunction TexFun;
+        public Byte[] UnkBytes1;
+        public Int32 TextureBasePointer { get; private set; }
+        public Int32[] MipLevelsTBP { get; private set; }
+        public Int32 TextureBufferWidth { get; private set; }
+        public Int32[] MipLevelsTBW { get; private set; }
+        public Int32 ClutBufferBasePointer { get; private set; }
+        public Byte[] UnkBytes2;
+        public Byte[] UnkBytes3;
+        public Byte[] UnusedMetadata {get; private set; }
         public Byte[] TextureData { get; private set; }
 
         public PS2AnyTexture()
         {
-            Header = new byte[96];
-            Metadata = new byte[32];
+            UnusedMetadata = new byte[32];
         }
 
         public UInt32 GetID()
@@ -33,15 +44,42 @@ namespace Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Graphics
 
         public Int32 GetLength()
         {
-            return 4 + Header.Length + Metadata.Length + (TextureData != null ? TextureData.Length : 0);
+            return 4 + 96 + UnusedMetadata.Length + (TextureData != null ? TextureData.Length : 0);
         }
 
         public void Read(BinaryReader reader, Int32 length)
         {
             int dataLen = reader.ReadInt32();
-            reader.Read(Header, 0, Header.Length);
-            reader.Read(Metadata, 0, Metadata.Length);
-            TextureData = reader.ReadBytes(dataLen - Header.Length - Metadata.Length);
+            HeaderSignature = reader.ReadUInt32();
+            ImageWidthPower = reader.ReadUInt16();
+            ImageHeightPower = reader.ReadUInt16();
+            MipLevels = reader.ReadByte();
+            TextureFormat = (TexturePixelFormat)reader.ReadByte();
+            DestinationTextureFormat = (TexturePixelFormat)reader.ReadByte();
+            ColorComponent = (TextureColorComponent)reader.ReadByte();
+            UnkByte = reader.ReadByte();
+            TexFun = (TextureFunction)reader.ReadByte();
+            UnkBytes1 = reader.ReadBytes(2);
+            TextureBasePointer = reader.ReadInt32();
+            MipLevelsTBP = new int[6];
+            for (var i = 0; i < 6; ++i)
+            {
+                MipLevelsTBP[i] = reader.ReadInt32();
+            }
+            TextureBufferWidth = reader.ReadInt32();
+            MipLevelsTBW = new int[6];
+            for (var i = 0; i < 6; ++i)
+            {
+                MipLevelsTBW[i] = reader.ReadInt32();
+            }
+            ClutBufferBasePointer = reader.ReadInt32();
+            UnkBytes2 = reader.ReadBytes(8);
+            reader.ReadInt32(); // Reserved
+            reader.ReadInt32(); // Reserved
+            UnkBytes3 = reader.ReadBytes(2);
+            reader.ReadBytes(2); // Reserved
+            reader.Read(UnusedMetadata, 0, UnusedMetadata.Length);
+            TextureData = reader.ReadBytes(dataLen - 96 - UnusedMetadata.Length);
         }
 
         public void SetID(UInt32 id)
@@ -52,10 +90,65 @@ namespace Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Graphics
         public void Write(BinaryWriter writer)
         {
             writer.Write(GetLength() - 4);
-            writer.Write(Header);
-            writer.Write(Metadata);
+            writer.Write(HeaderSignature);
+            writer.Write(ImageWidthPower);
+            writer.Write(ImageHeightPower);
+            writer.Write(MipLevels);
+            writer.Write((Byte)TextureFormat);
+            writer.Write((Byte)DestinationTextureFormat);
+            writer.Write((Byte)ColorComponent);
+            writer.Write(UnkByte);
+            writer.Write((Byte)TexFun);
+            writer.Write(UnkBytes1);
+            writer.Write(TextureBasePointer);
+            for (var i = 0; i < 6; ++i)
+            {
+                writer.Write(MipLevelsTBP[i]);
+            }
+            writer.Write(TextureBufferWidth);
+            for (var i = 0; i < 6; ++i)
+            {
+                writer.Write(MipLevelsTBW[i]);
+            }
+            writer.Write(ClutBufferBasePointer);
+            writer.Write(UnkBytes2);
+            writer.Write(0); // Reserved
+            writer.Write(0); // Reserved
+            writer.Write(UnkBytes3);
+            writer.Write((Int16)0); // Reserved
+            writer.Write(UnusedMetadata);
             writer.Write(TextureData);
-
         }
+
+        #region Enums
+        public enum TexturePixelFormat
+        {
+            PSMCT32 = 0b000000,
+            PSMCT24 = 0b000001,
+            PSMCT16 = 0b000010,
+            PSMCT16S = 0b001010,
+            PSMT8 = 0b010011,
+            PSMT4 = 0b010100,
+            PSMT8H = 0b011011,
+            PSMT4HL = 0b100100,
+            PSMT4HH = 0b101100,
+            PSMZ32 = 0b110000,
+            PSMZ24 = 0b110001,
+            PSMZ16 = 0b110010,
+            PSMZ16S = 0b111010
+        }
+        public enum TextureColorComponent
+        {
+            RGB = 0,
+            RGBA = 1
+        }
+        public enum TextureFunction
+        {
+            MODULATE = 0b00,
+            DECAL = 0b01,
+            HIGHLIGHT = 0b10,
+            HIGHLIGHT2 = 0b11
+        }
+        #endregion
     }
 }
