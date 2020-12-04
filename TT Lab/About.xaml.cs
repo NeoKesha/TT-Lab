cc9371f21348aebe4e37395bd8b7e363411d41b1
Smartkin <vladislav5019@gmail.com>
2020-12-04 23:12:02 +0300

    Finish main window design. Add About and Create Project windows

---
+++ TT Lab/About.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TT_Lab
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(Object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
---
+++ TT Lab/ProjectCreationWizard.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace TT_Lab
+{
+    /// <summary>
+    /// Interaction logic for ProjectCreationWizard.xaml
+    /// </summary>
+    public partial class ProjectCreationWizard : Window
+    {
+        public ProjectCreationWizard()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectPathButton_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
