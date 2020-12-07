90e169d3032750817aab81bc589d9eb2029a5dab
Smartkin <vladislav5019@gmail.com>
2020-12-07 19:54:05 +0300

    Add Undo/Redo interface

---
+++ TT Lab/Command/CommandManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace TT_Lab.Command
+{
+    public class CommandManager
+    {
+        private readonly Stack<ICommand> RedoStack = new Stack<ICommand>();
+        private readonly Stack<ICommand> UndoStack = new Stack<ICommand>();
+
+        public void Execute(ICommand command)
+        {
+            command.Execute();
+            UndoStack.Push(command);
+            RedoStack.Clear();
+        }
+
+        public void Undo()
+        {
+            if (UndoStack.Count == 0) return;
+
+            var undo = UndoStack.Pop();
+            undo.Unexecute();
+            RedoStack.Push(undo);
+        }
+
+        public void Redo()
+        {
+            if (RedoStack.Count == 0) return;
+
+            var redo = RedoStack.Pop();
+            redo.Execute();
+            UndoStack.Push(redo);
+        }
+    }
+}
---
+++ TT Lab/Command/ICommand.cs
@@ -0,0 +1,14 @@
+﻿namespace TT_Lab.Command
+{
+    public interface ICommand
+    {
+        /**
+         * Performs the command
+         */
+        void Execute();
+        /**
+         * Undoes the performed command
+         */
+        void Unexecute();
+    }
+}
