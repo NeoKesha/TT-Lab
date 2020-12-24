010b007b30b6ac8c8ce0926cb86612d1df27acfd
Neo_Kesha <neokesha@yandex.ru>
2020-12-25 01:02:46 +0300

    CollisionData

--- TT Lab/AssetData/Instance/CollisionSurfaceData.cs
+++ TT Lab/AssetData/Instance/CollisionSurfaceData.cs
@@ -17,10 +17,22 @@ namespace TT_Lab.AssetData.Instance
         public CollisionSurfaceData(PS2AnyCollisionSurface collisionSurface) : this()
         {
             SurfaceID = collisionSurface.SurfaceId;
+            Flags = collisionSurface.Flags;
+            StepSoundIds = (UInt16[])collisionSurface.StepSoundIds.Clone();
+            Parameters = (Single[])collisionSurface.Parameters.Clone();
+            UnkShorts = (UInt16[])collisionSurface.UnkShorts.Clone();
         }
 
         [JsonProperty(Required = Required.Always)]
         public UInt16 SurfaceID { get; set; }
+        [JsonProperty(Required = Required.Always)]
+        public UInt32 Flags { get; set; }
+        [JsonProperty(Required = Required.Always)]
+        public UInt16[] StepSoundIds { get; private set; }
+        [JsonProperty(Required = Required.Always)]
+        public Single[] Parameters { get; private set; }
+        [JsonProperty(Required = Required.Always)]
+        public UInt16[] UnkShorts { get; private set; }
 
         protected override void Dispose(Boolean disposing)
         {
