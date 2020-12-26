534cf2809e81a4c70094d463ffa0749437bdad6a
Smartkin <vladislav5019@gmail.com>
2020-12-26 19:55:35 +0300

    Add credits

--- TT Lab/Rendering/Shaders/Shader.cs
+++ TT Lab/Rendering/Shaders/Shader.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Credits to https://github.com/dwmkerr/sharpgl
 namespace TT_Lab.Rendering.Shaders
 {
     public class Shader
