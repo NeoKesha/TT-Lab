8cb988383e4d4ce591f810719c1b9da72ce6619d
Neo_Kesha <neokesha@yandex.ru>
2021-01-25 00:15:13 +0300

    remove texture testing shite

--- TT Lab/AssetData/Graphics/TextureData.cs
+++ TT Lab/AssetData/Graphics/TextureData.cs
@@ -70,34 +70,6 @@ namespace TT_Lab.AssetData.Graphics
                     BitsHandle.Free();
                 }
             }
-            twinRef = Export();
-            {
-                PS2AnyTexture texture = (PS2AnyTexture)twinRef;
-                if (texture.TextureFormat == TexturePixelFormat.PSMCT32 || texture.TextureFormat == TexturePixelFormat.PSMT8)
-                {
-                    Int32 width = (Int32)Math.Pow(2, texture.ImageWidthPower);
-                    Int32 height = (Int32)Math.Pow(2, texture.ImageHeightPower);
-                    texture.CalculateData();
-
-                    var Bits = new UInt32[width * height];
-                    var BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
-                    var tmpBmp = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
-
-                    for (var x = 0; x < width; ++x)
-                    {
-                        for (var y = 0; y < height; ++y)
-                        {
-                            var dstx = x;
-                            var dsty = height - 1 - y;
-                            Bits[dstx + dsty * width] = texture.Colors[x + y * width].ToARGB();
-                        }
-                    }
-
-                    Bitmap = new Bitmap(tmpBmp);
-                    tmpBmp.Dispose();
-                    BitsHandle.Free();
-                }
-            }
         }
 
         public override ITwinItem Export()
