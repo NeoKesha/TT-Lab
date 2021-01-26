a6d40ab589cd0c49c0e20050dab8d12d7c21d193
Neo_Kesha <neokesha@yandex.ru>
2021-01-26 21:46:11 +0300

    Lod export

--- TT Lab/AssetData/Graphics/LodModelData.cs
+++ TT Lab/AssetData/Graphics/LodModelData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TT_Lab.Assets.Graphics;
 using TT_Lab.Util;
 using Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.Graphics;
+using Twinsanity.TwinsanityInterchange.Interfaces;
 
 namespace TT_Lab.AssetData.Graphics
 {
@@ -49,6 +50,21 @@ namespace TT_Lab.AssetData.Graphics
             {
                 Meshes.Add(GuidManager.GetGuidByTwinId(mesh, typeof(Mesh)));
             }
+        }
+
+        public override ITwinItem Export()
+        {
+            PS2AnyLOD lod = new PS2AnyLOD();
+            lod.Type = Type;
+            lod.UnkInt1 = UnkInt1;
+            lod.UnkInt2 = UnkInt2;
+            lod.UnkInts = CloneUtils.CloneArray(UnkInts);
+            lod.UnkData = CloneUtils.CloneArray(UnkData);
+            foreach (var mesh in Meshes)
+            {
+                lod.Meshes.Add(GuidManager.GetTwinIdByGuid(mesh));
+            }
+            return lod;
         }
     }
 }
