94dc3f52e7e4b0dcd3c5360fc4be000bd33b25ec
Smartkin <vladislav5019@gmail.com>
2021-02-05 00:28:13 +0300

    Start on object instance editor

---
+++ TT Lab/Editors/Instance/ObjectInstanceEditor.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TT_Lab.Command;
+using TT_Lab.ViewModels.Instance;
+
+namespace TT_Lab.Editors.Instance
+{
+    /// <summary>
+    /// Interaction logic for ObjectInstanceEditor.xaml
+    /// </summary>
+    public partial class ObjectInstanceEditor : BaseEditor
+    {
+        public ObjectInstanceEditor()
+        {
+            InitializeComponent();
+        }
+
+        public ObjectInstanceEditor(ObjectInstanceViewModel vm, CommandManager commandManager) : base(vm, commandManager)
+        {
+            InitializeComponent();
+        }
+    }
+}
