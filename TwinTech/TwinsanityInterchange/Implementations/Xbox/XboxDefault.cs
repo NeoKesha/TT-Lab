337c9a5c13d4348099b490bed9540a0c1df0b80f
Smartkin <vladislav5019@gmail.com>
2023-10-01 02:40:20 +0300

    TwinTech: Forgor about XboxDefault

--- TwinTech/TwinsanityInterchange/Implementations/Xbox/XboxDefault.cs
+++ TwinTech/TwinsanityInterchange/Implementations/Xbox/XboxDefault.cs
@@ -1,6 +1,16 @@
-﻿namespace Twinsanity.TwinsanityInterchange.Implementations.Xbox
+﻿using Twinsanity.TwinsanityInterchange.Enumerations;
+using Twinsanity.TwinsanityInterchange.Implementations.Base;
+using Twinsanity.TwinsanityInterchange.Implementations.PS2.Items.RM2;
+using Twinsanity.TwinsanityInterchange.Implementations.Xbox.Items.RMX;
+
+namespace Twinsanity.TwinsanityInterchange.Implementations.Xbox
 {
     public class XboxDefault : XboxAnyTwinsanityRMX
     {
+        public XboxDefault() : base()
+        {
+            idToClassDictionary[Constants.LEVEL_PARTICLES_ITEM] = typeof(XboxDefaultParticleData);
+            idToClassDictionary[Constants.LEVEL_COLLISION_ITEM] = typeof(BaseTwinSection);
+        }
     }
 }
