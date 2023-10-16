75b6d6f3b708caca5857b9d8828ab7e2907d7e37
Smartkin <vladislav5019@gmail.com>
2023-10-17 00:57:07 +0300

    TwinTech: Implement XBox and PS2 frontend.bin support

---
+++ TwinTech/TwinsanityInterchange/Implementations/PS2/PS2Frontend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twinsanity.TwinsanityInterchange.Implementations.PS2.Sections.RM2.Code;
+
+namespace Twinsanity.TwinsanityInterchange.Implementations.PS2
+{
+    public class PS2Frontend : PS2AnySoundsSection
+    {
+    }
+}
---
+++ TwinTech/TwinsanityInterchange/Implementations/Xbox/XboxFrontend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twinsanity.TwinsanityInterchange.Implementations.Xbox.Sections.RMX.Code;
+
+namespace Twinsanity.TwinsanityInterchange.Implementations.Xbox
+{
+    public class XboxFrontend : XboxAnySoundsSection
+    {
+    }
+}
