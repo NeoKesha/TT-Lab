ec5d3853a7b833a262e71f12b1080a963a205db8
Smartkin <vladislav5019@gmail.com>
2023-10-17 00:22:36 +0300

    TwinTech: Xbox graphics section

--- TwinTech/TwinsanityInterchange/Implementations/Xbox/Sections/XboxAnyGraphicsSection.cs
+++ TwinTech/TwinsanityInterchange/Implementations/Xbox/Sections/XboxAnyGraphicsSection.cs
@@ -1,6 +1,22 @@
-﻿namespace Twinsanity.TwinsanityInterchange.Implementations.Xbox.Sections
+﻿using Twinsanity.TwinsanityInterchange.Enumerations;
+using Twinsanity.TwinsanityInterchange.Implementations.Base;
+using Twinsanity.TwinsanityInterchange.Implementations.Xbox.Sections.Graphics;
+
+namespace Twinsanity.TwinsanityInterchange.Implementations.Xbox.Sections
 {
-    internal class XboxAnyGraphicsSection
+    public class XboxAnyGraphicsSection : BaseTwinSection
     {
+        public XboxAnyGraphicsSection() : base()
+        {
+            idToClassDictionary.Add(Constants.GRAPHICS_TEXTURES_SECTION, typeof(XboxAnyTexturesSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_MATERIALS_SECTION, typeof(XboxAnyMaterialsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_MODELS_SECTION, typeof(XboxAnyModelsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_RIGID_MODELS_SECTION, typeof(XboxAnyRigidModelsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_SKINS_SECTION, typeof(XboxAnySkinsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_BLEND_SKINS_SECTION, typeof(XboxAnyBlendSkinsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_MESHES_SECTION, typeof(XboxAnyMeshesSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_LODS_SECTION, typeof(XboxAnyLODsSection));
+            idToClassDictionary.Add(Constants.GRAPHICS_SKYDOMES_SECTION, typeof(XboxAnySkydomesSection));
+        }
     }
 }
