13d4d7fe9c4d3c5d1ccebf690e01e0aeff5a98e4
Yashin Artem <neokesha@yandex.ru>
2024-01-21 23:46:39 +0300

    delete Matrix4ToArray

--- TT Lab/Util/MathExtension.cs
+++ TT Lab/Util/MathExtension.cs
@@ -17,14 +17,6 @@ namespace TT_Lab.Util
             return val;
         }
 
-        public static float[] Matrix4ToArray(Matrix4 matrix)
-        {
-            return new float[] { matrix.M11, matrix.M12, matrix.M13, matrix.M14,
-                                 matrix.M21, matrix.M22, matrix.M23, matrix.M24,
-                                 matrix.M31, matrix.M32, matrix.M33, matrix.M34,
-                                 matrix.M41, matrix.M42, matrix.M43, matrix.M44,};
-        }
-
         //https://gamedev.stackexchange.com/questions/96459/fast-ray-sphere-collision-code
         public static bool IntersectRaySphere(vec3 origin, vec3 direction, vec3 spherePosition, float radius, ref float distance, ref vec3 hit)
         {
